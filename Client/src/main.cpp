14ff8d2eab9db74c19ea6d2a4a27283503918bed
PapyRedstone <febvrealex@yahoo.fr>
2017-05-10 15:28:35 +0200

    supprimé :        ../AIBattleSimulator.out 	modifié :         ../Client/makefile 	modifié :         ../Client/src/main.cpp 	modifié :         makefile 	modifié :         src/main.cpp 	../Client/AIBattleSimulatorClient.out 	../Client/obj/

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,26 +1,27 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  // sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  // sf::CircleShape shape(100.f);
+  // shape.setFillColor(sf::Color::Green);
 
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      switch (event.type) {
-        case sf::Event::Closed:
-          window.close();
-          break;
+  // while (window.isOpen()) {
+  //   sf::Event event;
+  //   while (window.pollEvent(event)) {
+  //     switch (event.type) {
+  //       case sf::Event::Closed:
+  //         window.close();
+  //         break;
 
-        default:
-          break;
-      }
-    }
+  //       default:
+  //         break;
+  //     }
+  //   }
 
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
+  //   window.clear();
+  //   window.draw(shape);
+  //   window.display();
+  // }
   return 0;
 }
