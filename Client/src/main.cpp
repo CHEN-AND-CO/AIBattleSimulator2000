ca9f7b8076588b3d8fe488f5c29479be02ffdb96
PapyRedstone <febvrealex@yahoo.fr>
2017-05-10 16:05:34 +0200

    modifié :         Client/src/main.cpp 	modifié :         Serveur/src/main.cpp

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,27 +1,44 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
+#include <iostream>
 
 int main() {
-  // sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  // sf::CircleShape shape(100.f);
-  // shape.setFillColor(sf::Color::Green);
+  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
 
-  // while (window.isOpen()) {
-  //   sf::Event event;
-  //   while (window.pollEvent(event)) {
-  //     switch (event.type) {
-  //       case sf::Event::Closed:
-  //         window.close();
-  //         break;
+  sf::TcpSocket socket;
+  sf::Socket::Status status = socket.connect("127.0.0.1", 53000);
+  if (status != sf::Socket::Done)
+  {
+      std::cerr<<"Error open tcp\n";
+      return -1;
+  }
 
-  //       default:
-  //         break;
-  //     }
-  //   }
+  char data[100] = "Hello";
 
-  //   window.clear();
-  //   window.draw(shape);
-  //   window.display();
-  // }
+  // TCP socket:
+  if (socket.send(data, 100) != sf::Socket::Done)
+  {
+      // error...
+  }
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      switch (event.type) {
+        case sf::Event::Closed:
+          window.close();
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
   return 0;
 }
--- Serveur/src/main.cpp
+++ Serveur/src/main.cpp
@@ -1,10 +1,36 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
+#include <iostream>
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
+
+  sf::TcpListener listener;
+
+  // bind the listener to a port
+  if (listener.listen(53000) != sf::Socket::Done)
+  {
+      std::cerr<<"Error listener\n";
+      return -1;
+  }
+
+  // accept a new connection
+  
+
+  char data[100];
+  std::size_t received;
+
+  sf::TcpSocket socket;
+
+  sf::SocketSelector selector;
+
+  sf::TcpSocket client;
+
+  selector.add(socket);
+  selector.add(listener);
+
 
   while (window.isOpen()) {
     sf::Event event;
@@ -18,6 +44,17 @@ int main() {
           break;
       }
     }
+
+    if(selector.wait(sf::milliseconds(10))){
+      if(selector.isReady(listener)){
+        listener.accept(client);
+      }
+      else if(selector.isReady(socket))
+        socket.receive(data, 100, received);
+        std::cout << "Received " << data << " bytes" << std::endl;
+    }
+
+    
 
     window.clear();
     window.draw(shape);
