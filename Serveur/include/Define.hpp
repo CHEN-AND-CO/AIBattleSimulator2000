c271dc56d49b97fac1153e2119dd72672d7f45e6
PapyRedstone <febvrealex@yahoo.fr>
2017-05-16 10:43:31 +0200

    modifié :         include/Define.hpp 	modifié :         src/Define.cpp

--- Serveur/include/Define.hpp
+++ Serveur/include/Define.hpp
@@ -6,6 +6,7 @@ Define the type of the buildings and entities used in the game
 */
 
 #include <SFML/System.hpp>
+#include <cmath>
 
 enum class BuildingType { TownCenter, Fort, MaxBuildingType };
 enum class EntityType { Villager, Warrior, MaxEntityType };
@@ -17,5 +18,6 @@ enum class Ressource { Wood, MaxRessources };
 bool rectCollide(const sf::Vector2f& pos1, const sf::Vector2f& size1,
                  const sf::Vector2f& pos2, const sf::Vector2f& size2);
 bool rectCollide(const sf::Vector2f& pos1, const sf::Vector2f& pos2);
+bool rectInCircle(const sf::Vector2f pos1, int radius, sf::Vector2f pos2);
 
 #endif
